8283cee257b4ac2601c5f557e3ce89bf59d66bdb
Hasnain7200 <hs3962968@gmail.com>
2024-11-16 12:19:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import SignUp from './pages/SignUp';
+import InputPage from './pages/InputPage'
+
+import AdminSignUpPage from './pages/AdminSignUpPage';
+import AdminDataPage from './pages/AdminDataPage';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-[100%] h-[100%] bg-richblack-900 overflow-x-hidden">
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/admin' element={<Admin/>}></Route>
+        <Route path='/inputpage' element={<InputPage/>}></Route>
+        <Route path='/adminsignuppage' element={<AdminSignUpPage/>}></Route>
+        <Route path='/admindatapage' element={<AdminDataPage/>}></Route>
+      </Routes>
     </div>
   );
 }
